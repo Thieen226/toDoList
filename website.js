1514ed5dd252e3e16233e0b2720bfe4a10c33d1b
Thieen226 <t.ho1@my.metroed.net>
2023-10-20 07:57:43 -0700

    add

--- website.js
+++ website.js
@@ -2,15 +2,31 @@
 const addButton = document.getElementById("add");
 const toDosContainer = document.getElementById("toDosContainer");
 
-//add new list when clicking the button
 function newTasks(){ 
+//add new list when clicking the button
     const container = document.createElement("div");
     const pElement = document.createElement("p");
-    const newButton = document.createElement("button");
     const inputTask = document.getElementById("toDo").value; 
-    pElement.innerText = inputTask;
-    container.appendChild(pElement);
+    const doneButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+//adding child element in the toDosContainer and change the text inside of it based on what you put
     toDosContainer.appendChild(container);
+    container.appendChild(pElement);
+    pElement.innerText = inputTask;
+    container.appendChild(doneButton);
+    container.appendChild(removeButton);
+
+//adding class to the elements that are created in JavaScript
+    container.classList.add("container");
+    pElement.classList.add("pElement");
+    doneButton.classList.add("doneButton");
+    removeButton.classList.add("removeButton");
+
+//if you don't put anything in and add it, it will alert this text
+if (inputTask === ""){
+    alert("It is blank, you must write something!");
+}
 }
 addButton.addEventListener("click", newTasks); 
 
